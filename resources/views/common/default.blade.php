abdf26b6e31c6aaabdd4fcdbccd54076d118ea98
Jeremiah Ashley <jeremiah.ashley@live.com>
2017-06-16 00:22:53 -0500

    Sync

---
+++ resources/views/common/default.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>JermAshley</title>
+
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+
+        <navigation>
+
+            <div class="checker-red">
+                <div id="navLogo" class="">
+                    <img src="{{ asset('images/common/logo-black.png') }}" alt="">
+                </div>
+
+                <div id="navFirst" class="">
+
+                </div>
+
+                <div id="navLast" class="">
+
+                </div>
+            </div>
+
+        </navigation>
+
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
