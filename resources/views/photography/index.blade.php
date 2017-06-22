5bffca52a4c90c583864b811b61957a07f813002
Jeremiah Ashley <jeremiah.ashley@live.com>
2017-06-22 01:23:32 -0500

    Photography Page

---
+++ resources/views/photography/index.blade.php
@@ -0,0 +1,49 @@
+@extends('common.default')
+
+@section('mainContent')
+    @parent
+    <div class="pageContent">
+        <h1 class="pageHeading">Photography</h1>
+
+        <div class="d-flex flex-row justify-content-around flex-wrap">
+            <div class="d-flex flex-column">
+                <div class="photoStack" id="weddings">
+                    <div class="photoCard photo-1 dropShadow-1"><img src="https://unsplash.it/1010/?random" alt=""></div>
+                    <div class="photoCard photo-2-fog"></div>
+                    <div class="photoCard photo-2 dropShadow-1"><img src="https://unsplash.it/1020/?random" alt=""></div>
+                    <div class="photoCard photo-3-fog"></div>
+                    <div class="photoCard photo-3 dropShadow-1"><img src="https://unsplash.it/1030/?random" alt=""></div>
+                </div>
+                <div class="albumTitle">
+                    <h4>Wedding</h4>
+                </div>
+            </div>
+
+            <div class="d-flex flex-column">
+                <div class="photoStack" id="family">
+                    <div class="photoCard photo-1 dropShadow-1"><img src="https://unsplash.it/1010/?random" alt=""></div>
+                    <div class="photoCard photo-2-fog"></div>
+                    <div class="photoCard photo-2 dropShadow-1"><img src="https://unsplash.it/1020/?random" alt=""></div>
+                    <div class="photoCard photo-3-fog"></div>
+                    <div class="photoCard photo-3 dropShadow-1"><img src="https://unsplash.it/1030/?random" alt=""></div>
+                </div>
+                <div class="albumTitle">
+                    <h4>Family</h4>
+                </div>
+            </div>
+
+            <div class="d-flex flex-column">
+                <div class="photoStack" id="portraits">
+                    <div class="photoCard photo-1 dropShadow-1"><img src="https://unsplash.it/1010/?random" alt=""></div>
+                    <div class="photoCard photo-2-fog"></div>
+                    <div class="photoCard photo-2 dropShadow-1"><img src="https://unsplash.it/1020/?random" alt=""></div>
+                    <div class="photoCard photo-3-fog"></div>
+                    <div class="photoCard photo-3 dropShadow-1"><img src="https://unsplash.it/1030/?random" alt=""></div>
+                </div>
+                <div class="albumTitle">
+                    <h4>Portraits</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
