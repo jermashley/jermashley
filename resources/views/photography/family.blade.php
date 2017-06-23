a811cb5189b2a45012ccc699c4100428ce181984
Jeremiah Ashley <jeremiah.ashley@live.com>
2017-06-23 09:12:34 -0500

    Updated side-links on galleries

--- resources/views/photography/family.blade.php
+++ resources/views/photography/family.blade.php
@@ -9,11 +9,11 @@
             <div class="galleryLinks">
                 <ul class="list-unstyled">
                     <li><h5><a href="/photography/weddings">Wedding</a></h5></li>
-                    <li><h5 class="current">Family</h5></li>
+                    <li class="current"><h5>Family</h5></li>
                     <li><h5><a href="/photography/portraits">Portraits</a></h5></li>
                 </ul>
             </div>
-    
+
             <div class="gallerySlider">
                 <div class="photoStack">
                     <a href="#"><div class="photoCard photoCardGallery dropShadow-1"><img src="https://unsplash.it/1010/?random" alt=""></div></a>
@@ -37,9 +37,9 @@
                     <a href="#"><div class="photoCard photoCardGallery dropShadow-1"><img src="https://unsplash.it/1010/?random" alt=""></div></a>
                 </div>
             </div>
-            
+
         </div>
     </div>
-    
+
 
 @endsection
--- resources/views/photography/weddings.blade.php
+++ resources/views/photography/weddings.blade.php
@@ -8,12 +8,12 @@
         <div class="galleryContainer">
             <div class="galleryLinks">
                 <ul class="list-unstyled">
-                    <li><h5 class="current">Wedding</h5></li>
+                    <li class="current"><h5><a href="/photography/weddings">Wedding</a></h5></li>
                     <li><h5><a href="/photography/family">Family</a></h5></li>
                     <li><h5><a href="/photography/portraits">Portraits</a></h5></li>
                 </ul>
             </div>
-    
+
             <div class="gallerySlider">
                 <div class="photoStack">
                     <a href="#"><div class="photoCard photoCardGallery dropShadow-1"><img src="https://unsplash.it/1010/?random" alt=""></div></a>
@@ -37,9 +37,9 @@
                     <a href="#"><div class="photoCard photoCardGallery dropShadow-1"><img src="https://unsplash.it/1010/?random" alt=""></div></a>
                 </div>
             </div>
-            
+
         </div>
     </div>
-    
+
 
 @endsection
