ffdc9d87272b888c546af96fb28956c5f348496b
Jeremiah Ashley <jeremiah.ashley@live.com>
2017-06-25 10:13:41 -0500

    Added image to home page hero

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -3,16 +3,16 @@
 @section('mainContent')
     @parent
     <div class="d-flex flex-column justify-content-center homeHeroContainer">
-        <div class="homeHeroImage imageContainer dropShadow-1">
+        <div class="homeHeroImage imageContainer dropShadow-2">
             <div class="d-flex flex-column justify-content-center" style="height: 100%;">
                 <div class="homeHeroText d-flex flex-row justify-content-center">
-                    <h1>designers live to solve the problems we face daily - the mundane annoyances of life - in the most beautiful, elegant way possible.</h1>
+                    <h1 class="dropShadow-2">designers live to solve the problems we face daily - the mundane annoyances of life - in the most beautiful, elegant way possible.</h1>
                 </div>
             </div>
             <div class="imageComp gradientOverlay"></div>
             <div class="imageComp primaryBrandOverlay"></div>
             <div class="imageComp dimmer-1"></div>
-            <div class="imageComp imageBase"><img src="{{ asset('images/jet-light-trails.jpg') }}" alt=""></div>
+            <div class="imageComp imageBase"><img src="{{ asset('images/colored-clouds.jpg') }}" alt=""></div>
         </div>
     </div>
 @endsection
