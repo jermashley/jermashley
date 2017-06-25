ed50b1db3f5fb5c078c07fe150f61b81d724a500
Jeremiah Ashley <jeremiah.ashley@live.com>
2017-06-25 10:13:58 -0500

    Added contact content

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -5,19 +5,34 @@
     <div class="pageContent">
         <h1 class="pageHeading">Contact</h1>
 
-        <div class="container-fluid row">
-            <div class="col-lg-4 col-md-12 checker-red">
-                <div class="imageComp contactImage">
-                    <div class="imageComp gradientOverlay"></div>
-                    <div class="imageComp primaryBrandOverlay"></div>
-                    <div class="imageComp imageBase"><img src="{{ asset('images/jerm-portrait.jpg') }}" alt=""></div>
+        <div class="contactContainer">
+            <div class="contact-1">
+                <div class="">
+                    <div class="imageComp contactImage dropShadow-1">
+                        <div class="imageComp gradientOverlay"></div>
+                        <div class="imageComp primaryBrandOverlay"></div>
+                        <div class="imageComp imageBase"><img src="{{ asset('images/jerm-portrait.jpg') }}" alt=""></div>
+                    </div>
+                </div>
+                <div class="imageTitle">
+                    <h5>You can also reach me by e-mail at <a href="mailto:jerm@jermashley.me">jerm@jermashley.me</a>, or by using the form.</h5>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-12 checker-green">
-
+            <div class="contact-2 flex-first">
+                <div class="contactFormCard dropShadow-1">
+                    <form class="" action="index.html" method="post">
+                        <input name="name" type="text" class="" id="name" aria-describedby="name" placeholder="Your Name">
+                        <input name="email" type="email" class="" id="email" aria-describedby="email" placeholder="Email Address">
+                        <textarea name="message" type="text" id="message" aria-describedby="message" rows="5" placeholder="Type Your Message Here"></textarea>
+                        <button>Submit</button>
+                    </form>
+                </div>
             </div>
-            <div class="col-lg-4 col-md-12 checker-blue">
+            <div class="contact-3">
+                <h4>Quick About Me</h4>
+                <p>Plantronics with its GN Netcom wireless headset creates the next generation of wireless headset and other products such as wireless amplifiers, and wireless headset telephone.</p>
 
+                <p>You can get a wireless headset that is completely without wires or one that requires a belt pack. The ones that are completely wire free are made up of a headset worn on your head that talks to a base unit that is attached to your telephone with no wire connections between your headset and your phone. These wireless headset products represent the latest in sophisticated technology for communication or listening. Examples of wireless headset models are the Plantronics CS50, Plantronics CS55, the Plantronics 510S and the GN Netcom 9120.</p>
             </div>
         </div>
 
