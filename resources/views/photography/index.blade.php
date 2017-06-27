959fad2c3389e2bf0518cb211d6c226293a1a165
Jeremiah Ashley <jeremiah.ashley@live.com>
2017-06-26 23:49:43 -0500

    Removed unused structure

--- resources/views/photography/index.blade.php
+++ resources/views/photography/index.blade.php
@@ -10,10 +10,6 @@
                 <div class="d-flex flex-column">
                     <div class="photoStack" id="weddings">
                         <div class="photoCard photoCard-1"><img src="{{ asset('images/galleries/weddings/1-hughes_140921_173335_full-min.jpg') }}" alt=""></div>
-                        {{-- <div class="photoCard photoCard-2-fog"></div>
-                        <div class="photoCard photoCard-2"><img src="{{ asset('images/galleries/weddings/692A7991-min.jpg') }}" alt=""></div>
-                        <div class="photoCard photoCard-3-fog"></div>
-                        <div class="photoCard photoCard-3"><img src="{{ asset('images/galleries/weddings/692A6481-min.jpg') }}" alt=""></div> --}}
                     </div>
                     <div class="imageTitle">
                         <h4>Wedding</h4>
@@ -25,10 +21,6 @@
                 <div class="d-flex flex-column">
                     <div class="photoStack" id="portraits">
                         <div class="photoCard photoCard-1"><img src="{{ asset('images/galleries/portraits/692A7595-min.jpg') }}" alt=""></div>
-                        {{-- <div class="photoCard photoCard-2-fog"></div>
-                        <div class="photoCard photoCard-2"><img src="{{ asset('images/galleries/portraits/692A0067-HDR_watermark-min.jpg') }}" alt=""></div>
-                        <div class="photoCard photoCard-3-fog"></div>
-                        <div class="photoCard photoCard-3"><img src="{{ asset('images/galleries/portraits/hughes_140921_150518_full-min.jpg') }}" alt=""></div> --}}
                     </div>
                     <div class="imageTitle">
                         <h4>Portraits</h4>
@@ -40,10 +32,6 @@
                 <div class="d-flex flex-column">
                     <div class="photoStack" id="point-n-shoot">
                         <div class="photoCard photoCard-1"><img src="{{ asset('images/galleries/point-n-shoot/IMG_1677-min.jpg') }}" alt=""></div>
-                        {{-- <div class="photoCard photoCard-2-fog"></div>
-                        <div class="photoCard photoCard-2"><img src="{{ asset('images/galleries/point-n-shoot/692A5935-min.jpg') }}" alt=""></div>
-                        <div class="photoCard photoCard-3-fog"></div>
-                        <div class="photoCard photoCard-3"><img src="{{ asset('images/galleries/point-n-shoot/692A4457-min.jpg') }}" alt=""></div> --}}
                     </div>
                     <div class="imageTitle">
                         <h4>Point-n-Shoot</h4>
